b92e66783244ffee97fde3161a8271cc0ab8972d
Victor Oviedo <victor.oviedo.a87@gmail.com>
2022-07-29 15:20:45 -0300

    Empezando Back-end con model Persona

---
+++ victor/src/main/java/com/portfolio/victor/intrface/IPersonaService.java
@@ -0,0 +1,15 @@
+package com.portfolio.victor.intrface;
+
+import com.portfolio.victor.entity.Persona;
+import java.util.List;
+
+
+public interface IPersonaService {
+    public List<Persona> getPersona();
+    
+    public void savePersona(Persona persona);
+    
+    public void deletePersona(Long id);
+    
+    public Persona findPersona(Long id);
+}
